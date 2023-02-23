289beaf217ac9998860a157a60693523536cced3
BAMBAR00 <bryanathallah7@gmail.com>
2023-02-24 06:22:25 +0700

    first commit

---
+++ app/index.js
@@ -0,0 +1,9 @@
+const app = require('express')();
+
+app.get('/', (req, res) => { 
+  res.send("hello from a lightweight container!!! ")
+});
+
+app.listen(9999, () => {
+  console.log("server is running")
+})
